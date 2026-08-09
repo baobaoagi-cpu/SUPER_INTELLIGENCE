1a07fce33268b4c32a3df51518eaa49db5a3e968
baobaoagi-cpu <279388084+baobaoagi-cpu@users.noreply.github.com>
2026-08-09 14:31:52 +0800

    site update 2026/08/09 週日 14:31:52.17

--- 一鍵推送上線.bat
+++ 一鍵推送上線.bat
@@ -3,6 +3,7 @@ cd /d "%~dp0"
 echo.
 echo ==== Pushing website to GitHub ====
 echo.
+git rm --cached --ignore-unmatch -- *.bat >/dev/null 2>&1
 git add -A
 git commit -m "site update %date% %time%"
 git push
